48b0effbe2540f2eccfd32b55c93e784e9bdbbb9
LeonidShv <matembit1996@gmail.com>
2023-08-04 18:07:49 +0300

    infra: setup architecture for styles

---
+++ src/pages/HomePage/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <h1>Hello</h1>
+  </main>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+  h1 {
+    color: var(--text-color);
+
+    @include tablet-lower {
+      color: var(--brand-highlight);
+    }
+  }
+</style>
